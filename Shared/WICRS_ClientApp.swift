b9e6f08de630047ee25ec10571e74262b6332c9a
willemml <willem@leit.so>
2022-01-09 20:58:31 -0800

    Initial Commit

---
+++ Shared/WICRS_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  WICRS_ClientApp.swift
+//  Shared
+//
+//  Created by Willem Leitso on 2022-01-09.
+//
+
+import SwiftUI
+
+@main
+struct WICRS_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
